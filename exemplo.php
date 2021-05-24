bebee87ea3bb85664bece975ed078243c01ac6a1
claytoncsa <claytoncsa@gmail.com>
2021-05-24 01:48:41 -0300

    Busca inicial no ViaCep

--- exemplo.php
+++ exemplo.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "vendor/autoload.php";
+
 use Csa\DigitalCep\Search;
 
 $busca = new Search;
--- src/Search.php
+++ src/Search.php
@@ -6,12 +6,12 @@ class Search
 {
     private $url = "https://viacep.com.br/ws/";
 
-    public function getAddressFrom Zipcode(string $zipCode): array
+    public function getAddressFromZipcode(string $zipCode): array
     {
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/jason");
+        $get = file_get_contents($this->url.$zipCode."/json");
 
-        print_r($get);
+        return (array) json_decode($get);
     }
 }
